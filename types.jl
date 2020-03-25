5ad7a26d1fc089e6af5781b643fa55ab16be54a4
csimal <cedric.simal@student.unamur.be>
2020-03-25 16:52:06 +0100

    No stragglers

--- types.jl
+++
@@ -1,7 +0,0 @@
-using GraphRecipes # a graph vizualization package built ontop of Plots
-using Plots
-default(size=(1400,1000))
-
-plot(AbstractFloat, method=:tree, fontsize=12, nodeshape=:ellipse)
-
-savefig("types.png")
